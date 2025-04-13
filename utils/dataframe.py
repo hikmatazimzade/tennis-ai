882691ffea68fb37ccb8cc1c45cdc58c86c59187
Hikmat Azimzade <hikmetezimzade234@gmail.com>
2025-04-13 21:30:41 +0400

    Write dataset attaching scripts and concatenating datasets

--- utils/dataframe.py
+++ utils/dataframe.py
@@ -3,7 +3,8 @@ import pandas as pd
 from config import ROOT_DIR
 
 
-def attach_datasets(start_year: int=1968, end_year: int=2024) -> pd.DataFrame:
+def get_attached_dataset(start_year: int=1968,
+                         end_year: int=2024) -> pd.DataFrame:
     attached_df = []
     for year in range(start_year, end_year + 1):
         curr_df = pd.read_csv(f"{ROOT_DIR}/data/atp_matches_{year}.csv")
@@ -12,6 +13,15 @@ def attach_datasets(start_year: int=1968, end_year: int=2024) -> pd.DataFrame:
     return pd.concat(attached_df, ignore_index=True)
 
 
+def save_attached_dataset(start_year: int=1968,
+                         end_year: int=2024) -> None:
+    try:
+        attached_df = get_attached_dataset(start_year, end_year)
+        attached_df.to_csv(f"{ROOT_DIR}/data/atp_matches_train.csv")
+
+    except Exception as save_error:
+        print(f"Error occurred while saving attached dataset -> {save_error}")
+
+
 if __name__ == '__main__':
-    attached_df = attach_datasets()
-    print(attached_df.isnull().sum())
+    save_attached_dataset()
