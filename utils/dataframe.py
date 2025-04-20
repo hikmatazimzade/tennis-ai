ccf722a6c69af20a8a82e35a21924811ffceb05e
Hikmat Azimzade <hikmetezimzade234@gmail.com>
2025-04-20 11:32:10 +0400

    Pass save and get dataset methods to dataset module inside utils

--- utils/dataframe.py
+++ utils/dataframe.py
@@ -4,33 +4,9 @@ from random import randint
 import pandas as pd
 from pandas import DataFrame
 
-from config import ROOT_DIR
 from utils.logger import get_logger
 
-logger = get_logger("utils.logger")
-
-def get_attached_dataset(start_year: int=1968,
-                         end_year: int=2024) -> pd.DataFrame:
-    attached_df = []
-    for year in range(start_year, end_year + 1):
-        curr_df = pd.read_csv(f"{ROOT_DIR}/data/atp_matches_{year}.csv")
-        attached_df.append(curr_df)
-
-    return pd.concat(attached_df, ignore_index=True)
-
-
-def save_attached_dataset(start_year: int=1968,
-                         end_year: int=2024) -> None:
-    save_path = f"{ROOT_DIR}/data/atp_matches_train.csv"
-    try:
-        attached_df = get_attached_dataset(start_year, end_year)
-        attached_df.to_csv(save_path)
-        logger.info(f"Train dataset successfully saved to {save_path}")
-
-    except Exception as save_error:
-        logger.error(
-            f"Error occurred while saving attached dataset -> {save_error}"
-        )
+logger = get_logger("utils.dataframe")
 
 
 def get_shuffled_dataframe(df: DataFrame) -> DataFrame:
@@ -55,8 +31,6 @@ def get_remaining_cols(df: DataFrame) -> List[str]:
 
 
 if __name__ == '__main__':
-    save_attached_dataset()
-
     from data_processing.data_preprocessing import CleanDf
     class DummyCleaner(CleanDf):
         def handle_nan_seed_values(self):
---
+++ utils/dataset.py
@@ -0,0 +1,38 @@
+from typing import List
+from random import randint
+
+import pandas as pd
+from pandas import DataFrame
+
+from config import ROOT_DIR
+from utils.logger import get_logger
+
+logger = get_logger("utils.dataset")
+
+
+def get_attached_dataset(start_year: int=1968,
+                         end_year: int=2024) -> pd.DataFrame:
+    attached_df = []
+    for year in range(start_year, end_year + 1):
+        curr_df = pd.read_csv(f"{ROOT_DIR}/data/atp_matches_{year}.csv")
+        attached_df.append(curr_df)
+
+    return pd.concat(attached_df, ignore_index=True)
+
+
+def save_attached_dataset(start_year: int=1968,
+                         end_year: int=2024) -> None:
+    save_path = f"{ROOT_DIR}/data/atp_matches_train.csv"
+    try:
+        attached_df = get_attached_dataset(start_year, end_year)
+        attached_df.to_csv(save_path)
+        logger.info(f"Train dataset successfully saved to {save_path}")
+
+    except Exception as save_error:
+        logger.error(
+            f"Error occurred while saving attached dataset -> {save_error}"
+        )
+
+
+if __name__ == '__main__':
+    save_attached_dataset()
