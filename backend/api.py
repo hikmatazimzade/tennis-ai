6b3a6633fa9e5ca65d4604f5a70eb2612b7a68a9
Hikmat Azimzade <hikmetezimzade234@gmail.com>
2025-06-25 16:19:49 +0400

    Add base prediction endpoint

--- backend/api.py
+++ backend/api.py
@@ -1,8 +1,37 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 import uvicorn
+
+from backend.storage import (
+    PLAYER_DATA_DICT,
+    H2H_DICT,
+    SURFACE_H2H_DICT,
+    PREDICTION_COLUMNS
+)
 
 
 app = FastAPI()
+
+TOURNEY_YEAR = 2024
+TOURNEY_MONTH = 12
+TOURNEY_DAY = 15
+
+
+class Prediction(BaseModel):
+    player_1_entry: str = "ALT"
+    player_2_entry: str = "ALT"
+    surface: str = "Hard"
+    tourney_level: str = "A"
+    draw_size: str = 64
+
+
+@app.post("/prediction")
+def prediction(prediction: Prediction) -> dict:
+    print(prediction)
+    return {
+        "player_1_won": 1,
+        "confidence_score": 0.64
+    }
 
 
 if __name__ == '__main__':
